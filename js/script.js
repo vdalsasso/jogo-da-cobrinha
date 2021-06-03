4f5033f7779e3a7dd79a3fe83cf1428e5514ac22
vdalsasso <vin.dalsasso@gmail.com>
2021-06-03 17:11:08 -0300

    reiniciar jogo

--- js/script.js
+++ js/script.js
@@ -53,6 +53,8 @@ function iniciarJogo(){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
             alert('Game Over :(');
+
+            document.location.reload(true);
         }
     }
 
